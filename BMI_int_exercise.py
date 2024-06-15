5c29c6d46e33563c7e9a2150a6ad43019ba15a54
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-06-15 16:10:51 +0000

    Rounding in python

---
+++ BMI_int_exercise.py
@@ -0,0 +1,5 @@
+weight = input("Enter weight in kg: ")
+height = input("Enter height in m: ")
+
+BMI = int(weight) / (float(height) ** 2)
+print("You BMI is: ", int(BMI))
---
+++ rounding_in_python.py
@@ -0,0 +1,9 @@
+print(round(7))
+print(round(7, 2))
+print(round(7.66666, 2))
+print(round(7.66666))
+print(round(7.5))
+print(round(6.5))
+
+print(round(674, -2))
+print(round(674, -3))
