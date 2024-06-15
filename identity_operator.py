bc4be27bb8c6b490d00ccea67df155715c777a05
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-06-15 05:23:11 +0000

    membership operator

---
+++ identity_operator.py
@@ -0,0 +1,10 @@
+a = 5
+b = '5'
+
+print(a is b)
+print(a is not b)
+
+print(id(a))
+print(id(b))
+a = 6
+print(id(a))
---
+++ membership_operator.py
@@ -0,0 +1,8 @@
+str = 'Joshua Tulasi'
+print('Joshua' in str)
+
+print('J' not in str)
+print('b' not in str)
+
+l1 = [2, 67, 0, -7, 45]
+print(20 in l1)
