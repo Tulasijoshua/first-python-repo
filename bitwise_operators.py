670a497d8be315b1b7cb00c107f939f14e7495bc
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-06-15 04:50:50 +0000

    logical and bitwise operators

---
+++ bitwise_operators.py
@@ -0,0 +1,21 @@
+"""
+& and
+| or
+^ XOR
+~ NOT(COMPLEMENT)
+<< LEFT SHIFT
+>> RIGHT SHIFT
+"""
+a = 5
+b = 4
+
+print(a & b)
+print(a | b)
+print(a ^ b)
+
+print(~a)
+
+print(a<<2)
+print(a>>2)
+
+# Idendity operators
---
+++ logical_operators.py
@@ -0,0 +1,9 @@
+a,b=4,3
+
+print(a<3 and b==3)
+
+print(a<3 or b==3)
+print(not(a))
+
+c = True
+print(a<=4 and c)
