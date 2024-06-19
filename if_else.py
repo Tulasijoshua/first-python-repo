25680ec847a3a95cddb1b39f03411f3eaf8ec1e7
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-06-19 10:40:02 +0000

    Conditional statements in python

---
+++ if_else.py
@@ -0,0 +1,8 @@
+height = int(input("Enter height in feet: "))
+
+if (height > 3):
+    print("Buy Token")
+    print("Now you can board the metro")
+print("Out of if block")
+# else:
+#     print("No Token required")
