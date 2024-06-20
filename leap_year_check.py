4e32e248f77c7d5cb6923bebe5c8e9d2759defb5
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-06-20 05:35:36 +0000

    nested if bill exercise

---
+++ leap_year_check.py
@@ -0,0 +1,12 @@
+year = int(input("Enter year:  "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"The year {year} is s leap year")
+        else:
+            print(f"The year {year} is not a leap year")
+    else:
+        print("Not a leap year")
+else:
+    print("Not a leap year")
--- nested_if.py
+++ nested_if.py
@@ -1,14 +1,22 @@
 height = int(input("What is your height: "))
-
+bill = 0
 if height >= 3:
     print("Can ride")
     age = int(input("What is your age? "))
     if age < 12:
+        bill = 150
         print("Please pay 150 cedis")
     elif age <= 18:
+        bill = 250
         print("Please pay 250 cedis")
     else:
+        bill = 500
         print("Please pay 500 cedis")
+
+    want_photo = input("Do you want to take photo(Y/N)? ")
+    if want_photo == 'y' or want_photo == 'Y':
+        bill = bill + 50
+    print(f"Your total bill is {bill}")
 else:
     print("Can't ride")
 print("Bye!")
