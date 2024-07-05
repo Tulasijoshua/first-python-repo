fecb9038e6b8b0f0620d0053ec1e13025ea386cd
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-07-05 06:52:49 +0000

    average exercise

---
+++ average_height_exercise.py
@@ -0,0 +1,25 @@
+numbers = input("Enter list of numbers separated by space: ")
+
+numbers = numbers.split()
+sum = 0
+for i in numbers:
+    sum += int(i)
+else:
+    length = len(numbers)
+    height = sum/length
+    print("The average height is: " + round(height))
+
+# height_list = input("Enter a list separated by space: ")
+#
+# height_list = height_list.split()
+# count = 0
+# for height in height_list:
+#     count = count+1
+# for i in range(count):
+#     height_list[i] = int(height_list[i])
+#
+# total = 0
+# for person in height_list:
+#     total += person
+# avg = total/count
+# print(round(avg))
--- for_else_loop.py
+++ for_else_loop.py
@@ -2,12 +2,14 @@ tuple1 = (2, 56, 34, 3, 5, -1)
 
 for i in tuple1:
     print(i)
-    if i%6 == 0:
+    if i%6==0:
+        print(i)
         break
     # else:
-    #     print("There is no number divisible by 6 in this sequence")
-    # if i == 5:
-    #     break
+    #     print("There is no number divisible by 6 in this sequence!")
 else:
-    print("There is no number divisible by 6 in this sequence")
-    # print("Loop successfully completed and we are in else block now!!!")
+    print("There is no number divisible by 6 in this sequence!")
+
+print("Out of for/else!")
+
+# Else will not be executed when there is a break
