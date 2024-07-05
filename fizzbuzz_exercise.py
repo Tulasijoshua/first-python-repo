cea27bbd361a51e1158924874c36e909c26fe4fc
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-07-05 11:18:36 +0000

    fizzbuzz exercise

---
+++ fizzbuzz_exercise.py
@@ -0,0 +1,11 @@
+endpoint = int(input("Enter a number: "))
+
+for i in range(1, endpoint):
+    if i%3 == 0 and i%5 == 0:
+        print("FizzBuzz")
+    elif i%5 == 0:
+        print("Buzz")
+    elif i%3 == 0:
+        print("Fizz")
+    else:
+        print(i)
