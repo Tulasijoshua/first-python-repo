b402066d3cbdfbcaee58a75645d1656b164d27d9
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-07-20 03:25:22 +0000

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,35 @@
+phone_no = {
+    'Ram': 1234,
+    'Shyam': 3456,
+    'Mohan': 8766,
+}
+
+# phone_no = dict({
+#     'Ram': 112,
+#     'Shyam': 3456,
+#     'Mohan': 8766
+# })
+
+# phone_no = dict([('Ram', 432), ('Shyam', 3457), ('Mohan', 8976)])
+
+# print(phone_no)
+# print(phone_no['Ram'])
+
+# print(phone_no)
+# phone_no['Hadhav']= {1111, 2222, 3333}
+# phone_no['Shyam']={'Shyam_Home': 5555, 'Shyam_work': 4444}
+# print(phone_no['Shyam'])
+# print(phone_no['Shyam']['Shyam_work'])
+# print(phone_no['Shyam']['Shyam_work'])
+print(phone_no.get('ram'))
+print(phone_no.get('Ram'))
+
+data= {
+    1: 'jenny',
+    2: 'Ram',
+    0: 'Mohan'
+}
+
+# print(data[0])
+del phone_no['Ram']
+print(phone_no)
