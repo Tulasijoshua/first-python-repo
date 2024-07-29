fa205d13853c9735d23af90dd18b75333741da4b
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-07-29 23:21:46 +0000

    silent auction program complete

--- silent_auction_program.py
+++ silent_auction_program.py
@@ -1,16 +1,33 @@
+import os
+print("********Welcome to The Silent Auction Program*********")
+def find_winner(bidder_details):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidder_details:
+        bidding_price  = bidder_details[bidder]
+        if bidding_price > highest_bid:
+            highest_bid = bidding_price
+            winner = bidder
+    print(f"Her is the list of all the bidders {bidder_details}")
+    print(f"The winner is {winner} with a bid price of {highest_bid}")
+
 silent_bids = {}
-
-name = input('What is your name? ')
-bid = input('What is your bid? ')
-silent_bids[name] = bid
-print(silent_bids)
-
-another_bid = input('Do you have another bid? yes or no')
-
-while(another_bid == 'yes' or another_bid == 'Yes' or another_bid == 'YES'):
+end_of_bidding = False
+while not end_of_bidding:
     name = input('What is your name? ')
-    bid = input('What is your bid? ')
+    bid = int(input('What is your bid? '))
     silent_bids[name] = bid
-    another_bid = input('Do you have another bid? yes or no')
+    another_bid = input("Do you have another bid? 'yes' or 'no':").lower()
+    if another_bid == 'no':
+        end_of_bidding = True
+        find_winner(silent_bids)
+    elif another_bid == 'yes':
+        os.system('cls')
 
-print(silent_bids)
+# while(another_bid == 'yes'):
+#     name = input('What is your name? ')
+#     bid = input('What is your bid? ')
+#     silent_bids[name] = bid
+#     another_bid = input('Do you have another bid? yes or no')
+#
+# print(silent_bids)
