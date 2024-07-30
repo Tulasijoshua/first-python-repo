dd55d76db6ecf2f5ef3ab202deb270955e8adc31
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-07-30 04:02:58 +0000

    guess game and debugging tips

---
+++ debugging_tips.py
@@ -0,0 +1,29 @@
+# def display():
+#     for i in range(1, 10):
+#         if i==10:
+#             print("Bye!")
+# display()
+
+# import random
+# dice_numbers = ["One", "Two", "Three", "Four", "Five", "Six"]
+# dice_num = random.randint(1, 5)
+# print(dice_numbers[dice_num])
+
+# year = int(input("In which year you were born? "))
+# if year>1980 and year <= 1996:
+#     print("You are a Millenial")
+# elif year > 1996:
+#     print("You are Gen Z")
+
+# age= int(input("How old are you? "))
+# print(type(age))
+# if age >= 18:
+#     print("You can vote at {age}")
+
+a,b=0,0
+a=int(input("Enter a: "))
+print(f"a is {a}")
+b==int(input("Enter b: "))
+print(f"b is {b}")
+multiplication=a*b
+print(multiplication)
