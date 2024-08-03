0acc4dfb86de7569bda8a8d78e22110862b063d2
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-08-03 11:17:41 +0000

    inheritance in python

---
+++ inheritance_intro.py
@@ -0,0 +1,30 @@
+class Human:
+    def __init__(self, num_heart):
+        self.num_eyes = 2
+        self.num_nose = 1
+        self.num_heart = num_heart
+    def eat(self):
+        print("I can eat")
+    def work(self):
+        print("I can work")
+
+class Male(Human):
+    def __init__(self, name, heart):
+        super().__init__(heart)
+        self.name = name
+        self.heart = heart
+    def flirt(self):
+        print("I can flirt")
+    def work(self):
+        print("I can code")
+    def eat(self):
+        super().eat()
+        print("I can eat fufu")
+    def display(self):
+        print(f"Hi, I am {self.name} and I have {self.num_heart} heart.")
+
+male_1 = Male("Tulasi", 1)
+print(male_1.name)
+print(male_1.num_eyes)
+print(f"Male has {male_1.num_heart}")
+male_1.display()
