332f3c0cc55698ae6ed2d70af5dd9e97e7d28baa
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-09-25 09:08:16 +0000

    calculating students GPA

--- GPA_calculation.py
+++ GPA_calculation.py
@@ -3,27 +3,30 @@ sub_marks = sub_marks.split(',')
 
 total_grade_points = 0
 grade = 0
+def grade_point(i):
+    if i >= 80:
+        return 4.0
+    elif i >= 75:
+        return 3.5
+    elif i >= 70:
+        return 3.0
+    elif i >= 65:
+        return 2.5
+    elif i >= 60:
+        return 2.0
+    elif i >= 55:
+        return 1.5
+    elif i >= 50:
+        return 1
+    else:
+        return 0.5
+
+
 total_credit_unit = len(sub_marks) * 3
 if len(sub_marks) == 5:
     for i in sub_marks:
-        if int(i) >= 80:
-            i = 4.0
-        elif int(i) >= 75:
-            i = 3.5
-        elif int(i) >= 70:
-            i = 3.0
-        elif int(i) >= 65:
-            i = 2.5
-        elif int(i) >= 60:
-            i = 2.0
-        elif int(i) >= 55:
-            i = 1.5
-        elif int(i) >= 50:
-            i = 1
-        else:
-            i = 0.5
-
-        total_grade_points += i
+        grade_point_value = grade_point(int(i))
+        total_grade_points += grade_point_value
     grade = (total_grade_points * 3)/total_credit_unit
     print(grade)
 else:
