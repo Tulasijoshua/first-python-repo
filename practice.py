85cb042b35b5a3533fde0dd76da759b9cb0e9486
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-09-25 10:32:08 +0000

    loop

--- practice.py
+++ practice.py
@@ -1,17 +1,8 @@
-year = int(input("Enter a year: "))
-def leap_year(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                False
-        else:
-            return True
-    else:
-        return False
-
-if leap_year(year):
-    print(f"The year {year} is a leap year!")
-else:
-    print(f"The year {year} is not a leap year!!!")
+count = 1
+while count <= 10:
+    print(count)
+    count += 1
+    if count == 7:
+        continue
+    print("Hi")
+print("Out from loop")
